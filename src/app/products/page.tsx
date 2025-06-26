c2d8266a3e281b4288c4da4801dae3cb6690b012
Ian Souza <ian64208@gmail.com>
2025-06-26 14:25:15 -0300

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { AlertDialog, AlertDialogTrigger, AlertDialogContent, AlertDialogTitle, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogAction, AlertDialogCancel } from "@/components/ui/alert-dialog"
 import { Button } from "@/components/ui/button"
 import { PlusCircle } from "lucide-react"
+import Header from "@/components/ui/header"
 export default function Home() {
 
   const formCadastrarProduto = () => {
@@ -35,62 +36,64 @@ export default function Home() {
   }
 
   return (
-    <div className="p-4 bg-gray-50 min-h-screen w-full flex flex-col">
-      <div className="flex justify-between mb-4 p-4">
-        <h1 className="text-4xl font-bold text-black">Produtos</h1>
-        {formCadastrarProduto()}
-      </div>
-      <div className="flex gap-4 p-4 bg-gray-100 border rounded-md">
-        <div className="bg-gray-200 h-full rounded-md p-4">
-          <SidebarProvider className="flex flex-col">
-            <SidebarHeader>
-              <h2 className="text-lg font-bold">Categorias</h2>
-            </SidebarHeader>
-            <SidebarContent>
-              <SidebarGroup>
-                <SidebarMenu>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton>
-                      <span>Todos os Produtos</span>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton>
-                      <span>Eletrônicos</span>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton>
-                      <span>Roupas</span>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton>
-                      <span>Calçados</span>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                </SidebarMenu>
-              </SidebarGroup>
-            </SidebarContent>
-          </SidebarProvider>
+    <>
+      <div className="p-4 bg-gray-50 min-h-screen w-full flex flex-col">
+        <div className="flex justify-between mb-4 p-4">
+          <h1 className="text-4xl font-bold text-black">Produtos</h1>
+          {formCadastrarProduto()}
         </div>
-        <div className="bg-gray-200 w-full h-full rounded-md p-4">
-          <Card className="h-full">
-            <CardHeader>
-              <CardTitle className="text-2xl font-bold">Produtos em Destaque</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="grid grid-cols-[repeat(auto-fit,minmax(225px,1fr))] gap-8">
-                <p>Produto 1</p>
-                <p>Produto 2</p>
-                <p>Produto 3</p>
-                <p>Produto 4</p>
-              </div>
-            </CardContent>
-          </Card>
+        <div className="flex gap-4 p-4 bg-gray-100 border rounded-md">
+          <div className="bg-gray-200 h-full rounded-md p-4">
+            <SidebarProvider className="flex flex-col">
+              <SidebarHeader>
+                <h2 className="text-lg font-bold">Categorias</h2>
+              </SidebarHeader>
+              <SidebarContent>
+                <SidebarGroup>
+                  <SidebarMenu>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton>
+                        <span>Todos os Produtos</span>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton>
+                        <span>Eletrônicos</span>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton>
+                        <span>Roupas</span>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton>
+                        <span>Calçados</span>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  </SidebarMenu>
+                </SidebarGroup>
+              </SidebarContent>
+            </SidebarProvider>
+          </div>
+          <div className="bg-gray-200 w-full h-full rounded-md p-4">
+            <Card className="h-full">
+              <CardHeader>
+                <CardTitle className="text-2xl font-bold">Produtos em Destaque</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid grid-cols-[repeat(auto-fit,minmax(225px,1fr))] gap-8">
+                  <p>Produto 1</p>
+                  <p>Produto 2</p>
+                  <p>Produto 3</p>
+                  <p>Produto 4</p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 
 }
