bf69b3f936f8124ba2c491c9b27e67b02b8c0093
Gabriel Fernandes de Freitas Moreira Duarte de Lima <gabrielfernandes2005@gmail.com>
2025-06-25 21:21:25 -0300

    adwad

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['localhost', 'via.placeholder.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-  transpilePackages: ['lucide-react'],
+  /* config options here */
 };
 
 export default nextConfig;
