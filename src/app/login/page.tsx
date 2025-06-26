53ccd5a5a51d796a19745fcbd8d32e4cc6562ce8
Gabriel Fernandes de Freitas Moreira Duarte de Lima <gabrielfernandes2005@gmail.com>
2025-06-25 22:32:33 -0300

    revisao11

---
+++ src/app/login/page.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import axios from "axios";
+
+
+export default function Home({ user, setUser }: { user: any; setUser: any }) {
+
+    const [email, setEmail] = useState<string>();
+    const [password, setPassword] = useState<string>();
+    const [redirect, setRedirect] = useState<boolean>(false);
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (email && password) {
+            try {
+                const { data: userDoc } = await axios.post("/users/login", {
+                    email,
+                    password,
+                });
+
+                setUser(userDoc);
+                setRedirect(true);
+            } catch (e: any) {
+                alert(`Erro ao fazer login: ${e.response.data}`);
+            }
+        } else {
+            alert("Preencha o e-mail e a senha!");
+        }
+    };
+
+    if (redirect || user) {
+        return <Navigate to="/" />;
+    }
+
+    return (
+        <section className="flex items-center">
+            <div className="mx-auto flex w-full max-w-96 flex-col items-center gap-4">
+                <h1 className="py-2 text-3xl font-bold">Faça seu Login</h1>
+
+                <form className="flex w-full flex-col gap-3" onSubmit={handleSubmit}>
+                    <input
+                        type="email"
+                        placeholder="Digite seu e-mail"
+                        className="w-full rounded-full border border-gray-400 px-4 py-4"
+                        value={email}
+                        onChange={(e: any) => {
+                            setEmail(e.target.value);
+                        }}
+                    ></input>
+                    <input
+                        type="password"
+                        placeholder="Digite sua senha"
+                        className="w-full rounded-full border border-gray-400 px-4 py-4"
+                        value={password}
+                        onChange={(e: any) => {
+                            setPassword(e.target.value);
+                        }}
+                    ></input>
+                    <button className="bg-primary-400 w-full cursor-pointer rounded-full border border-gray-400 px-4 py-4 font-bold text-white">
+                        Login
+                    </button>
+                </form>
+
+                <p>
+                    Ainda não tem uma conta?{" "}
+                    <Link to="/register" className="font-semibold underline">
+                        Registre-se Aqui!
+                    </Link>
+                </p>
+            </div>
+        </section>
+    );
+}
---
+++ src/components/ui/header.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "./dropdown-menu";
+import { ShoppingCart, CircleUser, Menu } from "lucide-react";
+
+
+const Header = ({ user }: { user: any }) => {
+    return (
+        <header className="shadow-md fixed top-0 left-0 w-full z-10">
+            <div className="max-w-7x1 mx-auto flex items-center justify-between sm:px-8 py-8 bg-red-800">
+                <Link href="/" className="flex items-center">
+                    <img className="h-10" src="" alt="" />
+                    <p className="text-white text-2xl font-bold">
+                        Barãousada
+                    </p>
+                </Link>
+
+
+                <div className="flex items-center gap-3 rounded-full border border-white py-2 pr-4 pl-6 shadow-md">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger>
+                            <Menu size={34} color="#ffffff"/>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem>
+                                <Link href={user ? "/account" : "/login"}>{user ? "Minha Conta" : "Login"}</Link>
+                            </DropdownMenuItem>
+                            {user ? (
+                                <>
+                                    <DropdownMenuItem>
+                                        <Link href="/products">Meus Produtos</Link>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem>
+                                        <Link href="/orders">Meus Pedidos</Link>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem>
+                                        <Link href="/logout">Sair</Link>
+                                    </DropdownMenuItem>
+                                </>
+                            ) : null}
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+
+                    <CircleUser size={34} color="#ffffff"/>
+                    {user ? (
+                        <p className="trun max-w-20 sm:max-w-32">{user.name}</p>
+                    ) : (
+                        <></>
+                    )}
+
+                    <ShoppingCart size={34} color="#ffffff"/>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
